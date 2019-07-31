ef5685e70c5cba98423ffb974a5817b48ce785b4
Niccolò Musso Barcucci <niccolo.mussobarcucci@gmail.com>
2019-07-31 20:09:23 +0200

    Update randomdmx.ino

--- randomdmx.ino
+++ randomdmx.ino
@@ -11,9 +11,9 @@
 #define BUTTON3 D5
 #define LED A0
 
-int value1 = 101;
-int value2 = 102;
-int value3 = 103;
+byte value1;
+byte value2;
+byte value3;
 
 
 const char *ssid = "TRUMENBUTTONS";
@@ -31,19 +31,19 @@ void handleRoot() {
   info+= "<form enctype='application/json' action='/change' >";
   info+= "  BUTTONE 1 <input type='number' name='BUTTON1' value='";
   info+=value1;
-  info+="Attuale: ";
+  info+="'>Attuale: ";
   info+=value1;
   info+="<br>";
   info+= "  BUTTONE 2 <input type='number' name='BUTTON2' value='";
   info+=value2;
-  info+="Attuale: ";
+  info+="'>Attuale: ";
   info+=value2;
-  info+="'><br>";
+  info+="<br>";
   info+= "  BUTTONE 3 <input type='number' name='BUTTON3' value='";
   info+=value3;
-  info+="Attuale: ";
+  info+="'>Attuale: ";
   info+=value3;
-  info+="'><br>";
+  info+="<br>";
   info+= "  <input type='submit' value='Salva Configurazione'>";
   info+= "</form>";
   server.send(200, "text/html", info);
@@ -54,13 +54,15 @@ void changeConfiguration(){
   value1 = server.arg("BUTTON1").toInt();
   value2 = server.arg("BUTTON2").toInt();
   value3 = server.arg("BUTTON3").toInt();
+  EEPROM.write(0,value1);
+  EEPROM.write(1,value2);
+  EEPROM.write(2,value3);
+  EEPROM.commit();
   server.sendHeader("Location","/");
   server.send(303);
 }
 
 void setup() {
-  EEPROM.begin(512);
-  
   dmx.init(512);
   pinMode(BUTTON1, INPUT);
   pinMode(BUTTON2, INPUT);
@@ -80,6 +82,23 @@ void setup() {
   server.on("/change", changeConfiguration);
   server.begin();
   Serial.println("HTTP server started");
+
+  EEPROM.begin(4);
+  value1=EEPROM.read(0);
+  value2=EEPROM.read(1);
+  value3=EEPROM.read(2);
+
+  Serial.print("test");
+  Serial.print(value1);
+
+  
+  Serial.write("data Recovered from internal memory:\n");
+  Serial.write("Button1: ");
+  Serial.print(value1);
+  Serial.write("Button2: ");
+  Serial.print(value2);
+  Serial.write("Button3: ");
+  Serial.print(value3);
 }
 
 void loop() {
